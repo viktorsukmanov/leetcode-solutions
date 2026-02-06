fa6218672f612c76a023a01e26b52de033a44e2f
viktorsukmanov <viktorsukmanov.dev@gmail.com>
2026-02-06 04:41:57 +0300

    Time: 22 ms (86.2%), Space: 61 MB (5.29%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         Arrays.sort(points, (p1, p2) -> p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1]);
-        int [][] result = new int[k][];
+        int [][] answer = new int[k][];
         for(int i = 0; i < k; i++){
-            result[i] = points[i];
+            answer[i] = points[i];
         }
-        return result;
+        return answer;
     }
 }
