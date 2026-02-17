b35e559b67f47d890ed23bf1a48c01586b8051ca
viktorsukmanov <viktorsukmanov.dev@gmail.com>
2026-02-17 08:44:49 +0300

    Time: 70 ms (20.81%), Space: 48.1 MB (11.27%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,47 +1,55 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-            List<Integer> answer = new ArrayList<>();
+        List<Integer> answer = new ArrayList();
         if(s.length() < p.length()){
             return answer;
         }
 
-        Map<Character, Integer> counterP = new HashMap<>();
-        Map<Character, Integer> counterS = new HashMap<>();
+        Map<Character, Integer> sCounter = new HashMap<>();
+        Map<Character, Integer> pCounter = new HashMap<>();
         for(int i = 0; i < p.length(); i++){
-            counterP.put(p.charAt(i), counterP.getOrDefault(p.charAt(i), 0) + 1);
+            sCounter.put(s.charAt(i), sCounter.getOrDefault(s.charAt(i),0) + 1);
         }
         for(int i = 0; i < p.length(); i++){
-            counterS.put(s.charAt(i), counterS.getOrDefault(s.charAt(i), 0) + 1);
+            pCounter.put(p.charAt(i), pCounter.getOrDefault(p.charAt(i),0) + 1);
+        }
+    
+        if(sameCounters(sCounter, pCounter)){
+        answer.add(0);
+         }
+
+        int left = 0;
+        int right = p.length() - 1;
+
+        while(right + 1 < s.length()){
+        Character charToAdd = s.charAt(right + 1);
+        Character charToRemove = s.charAt(left);
+        sCounter.put(charToAdd, sCounter.getOrDefault(charToAdd,0)+1);
+        sCounter.put(charToRemove, sCounter.get(charToRemove) - 1);
+        left++;
+        right++;
+        if(sameCounters(sCounter, pCounter)){
+            answer.add(left);
         }
 
-        if(counterP.equals(counterS)) answer.add(0);
-
-        int l = 0;
-        int r = p.length() - 1;
-        while(r + 1 < s.length()){
-            Character charToAdd = s.charAt(r + 1);
-            Character charToRemove = s.charAt(l);
-            counterS.put(charToAdd, counterS.getOrDefault(charToAdd, 0) + 1);
-            counterS.put(charToRemove, counterS.get(charToRemove) - 1);
-            r++;
-            l++;
-            if(sameCounters(counterP, counterS)) answer.add(l);
-        }
-        return answer;
     }
-
-    private boolean sameCounters(Map<Character, Integer> counter1, Map<Character, Integer> counter2){
-        for(Map.Entry<Character, Integer> entry : counter1.entrySet()){
-            if(!counter2.getOrDefault(entry.getKey(), 0).equals(entry.getValue())){
-                return false;
-            }
-        }
-
-        for(Map.Entry<Character, Integer> entry : counter2.entrySet()){
-            if(!counter1.getOrDefault(entry.getKey(), 0).equals(entry.getValue())){
-                return false;
-            }
-        }
-        return true;
+    return answer;
     }
+    private boolean sameCounters( Map<Character, Integer> counter1,
+                         Map<Character, Integer> counter2){
+
+            for(Map.Entry<Character, Integer> entry : counter1.entrySet()){
+                if(!counter2.getOrDefault(entry.getKey(),0).equals(entry.getValue())){
+                    return false;
+                }
+            }
+
+            for(Map.Entry<Character, Integer> entry : counter2.entrySet()){
+                if(!counter1.getOrDefault(entry.getKey(), 0).equals(entry.getValue())){
+                    return false;
+                }
+            }
+            return true;
+    }
+    
 }
